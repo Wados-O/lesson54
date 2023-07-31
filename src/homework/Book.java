8ac9c94a112662f133a80e195cdff5823d54c0f7
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-07-31 23:39:29 +0200

    feat: homework add Book

---
+++ src/homework/Book.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class Book {
+  private String name;
+  private String authors;
+  private int lists;
+
+  public Book(String name, String authors, int lists) {
+    this.name = name;
+    this.authors = authors;
+    this.lists = lists;
+  }
+}
