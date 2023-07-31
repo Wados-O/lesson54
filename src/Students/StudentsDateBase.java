5690241df85d1ac8bb01be67c0fb48b864585b4b
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-07-31 20:57:39 +0200

    feat : add student list methods and Student template

--- src/Students/StudentsDateBase.java
+++ src/Students/StudentsDateBase.java
@@ -3,31 +3,36 @@ package Students;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentsDateBase {
 
   public static void main(String[] args) throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    printMenu();
-    String command = reader.readLine();
+    List<Student> students = new ArrayList<>();
+
+    String command = readMenuCommand(reader);
     while (!command.equalsIgnoreCase("выход")) {
       switch (command.toLowerCase()) {
         case "добавить":
-          System.out.println("Добавляем студента");
+          addStudentInteractive(students);
+
           break;
         case "сохранить":
-          System.out.println("Сохраняем список");
+          saveListStudents(students);
+
           break;
-        case "вывод":
-          System.out.println("Выводим список на экран");
+        case "вывести":
+
           break;
         default:
           System.out.println(" Неизвестная команда " + command);
           break;
       }
 
-      printMenu();
-      command = reader.readLine();
+
+      command = readMenuCommand(reader);
     }
 
   }
@@ -38,5 +43,23 @@ public class StudentsDateBase {
     System.out.println("- 'Вывод' списка студентов на экран");
     System.out.println("- 'Выход' из программы");
 
+  }
+  private static String readMenuCommand(BufferedReader reader) throws IOException {
+    printMenu();
+    return reader.readLine();
+  }
+  private static void addStudentInteractive(List<Student> students){
+    Student student = new Student();
+    students.add(student);
+    System.out.println("Добавляем студента" +student);
+  }
+  private static void saveListStudents(List<Student> students){
+    System.out.println("Сохраняем список" + students);
+  }
+  private static void printStudentsList(List<Student> students){
+    System.out.println("Выводим список на экран");
+    for (Student s : students){
+      System.out.println(s);
+    }
   }
 }
