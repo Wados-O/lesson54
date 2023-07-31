29dc8f53ba6d8afb9c5709ddd709307a359deb11
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-07-31 19:16:52 +0200

    feat : override equals hashCode

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,10 @@ public class Main {
 
   public static void main(String[] args) {
     List<Pizza> pizzas = new ArrayList<>();
-    pizzas.add(new Pizza("A","S",3));
-    pizzas.add(new Pizza("B","M",5));
-    pizzas.add(new Pizza("A","SX",7));
+    pizzas.add(new Pizza("A","L",25));
+    pizzas.add(new Pizza("A","S",17));
+    pizzas.add(new Pizza("B","M",9));
+    pizzas.add(new Pizza("A","SX",15));
 
     System.out.println("До сортировки:");
     for (Pizza p : pizzas){
--- src/Pizza.java
+++ src/Pizza.java
@@ -2,89 +2,20 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Pizza implements Comparable<Pizza> {
-  // интерфейс Comparable<Класс> говорит, что объекты этого класса (и наследников) можно сравнивать
-  // В этом интерфейсе обязательно нужно реализовать метод `compareTo()`
-
-  private static final String SEP = ",";
-  // Map<НазваниеПиццы, Map<Размер, Стоимость>>
-  private static final Map<String, Map<String, Double>> prices = readFromCsv("res/pizzas.csv");
 
   private final String name;
   private final String size;
   private final double price;
 
-  public Pizza(String name, String size) {
-    if (!prices.containsKey(name)) {
-      throw new IllegalArgumentException("Некорректное название пиццы: " + name);
-    }
+  public Pizza(String name, String size, double price) {
     this.name = name;
-    Map<String, Double> sizeToPrice = prices.get(name); // стоимость в зависимости от размера
-    if (!sizeToPrice.containsKey(size)) {
-      throw new IllegalArgumentException("Некорректный размер: " + size);
-    }
     this.size = size;
-    price = sizeToPrice.get(size);
-  }
-
-  private static String readValueFromSet(Scanner scanner, Set<String> values, String title) {
-    System.out.println("Выберите " + title + ":");
-    for (String name : values) {
-      System.out.println("- " + name);
-    }
-    System.out.print("Введите " + title + ": ");
-    String value = scanner.nextLine();
-    while (!values.contains(value)) {
-      System.out.println("Некорректное " + title + " пиццы: " + value);
-      System.out.print("Введите " + title + ": ");
-      value = scanner.nextLine();
-    }
-    return value;
-  }
-
-  // фабричный метод, прочитает параметры пиццы в интерактивном режиме
-  public static Pizza readInteractive(Scanner scanner) {
-    String name = readValueFromSet(scanner, prices.keySet(), "название");
-    String size = readValueFromSet(scanner, prices.get(name).keySet(), "размер");
-    return new Pizza(name, size);
-  }
-
-  private static Map<String, Map<String, Double>> readFromCsv(String filename) {
-    // CSV - comma separated values - значения, разделённые запятыми -
-    // самый простой формат таблиц.
-    Map<String, Map<String, Double>> result = new HashMap<>();
-    File pizzasFile = new File(filename);
-    try {
-      Scanner scanner = new Scanner(pizzasFile);
-      while (scanner.hasNextLine()) {
-        // line = "Capricciosa,Small,7.5"
-        String line = scanner.nextLine();
-        // cells = ["Capricciosa", "Small", "7.5"]
-        String[] cells = line.split(SEP);
-        try {
-          // pizza = "Capricciosa"
-          String pizza = cells[0];
-          // size = "Small"
-          String size = cells[1];
-          // price = Double.parseDouble("7.5") = 7.5
-          double price = Double.parseDouble(cells[2]);
-          if (!result.containsKey(pizza)) { // пицца встретилась первый раз
-            result.put(pizza, new HashMap<>()); // кладём ей пока пустой словарь "размер-цена"
-          }
-          // теперь словарь "размер-цена" для пиццы точно есть в нашем словаре
-          result.get(pizza).put(size, price);
-        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-          System.out.println("Некорректная строка файла: " + line);
-        }
-      }
-      scanner.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("Не найден файл: " + e);
-    }
-    return result;
+    this.price = price;
   }
 
   @Override
@@ -114,13 +45,30 @@ public class Pizza implements Comparable<Pizza> {
     if (!name.equals(o.name)) { // (compareTo != 0) --> названия не равны (разные)
       return name.compareTo(o.name); // по названиям
     }
+    if (!size.equals(o.size)) {
+      return name.compareTo(o.size);
+    }
     // если названия одинаковые, то по цене
     // (double - double) нельзя превращать в int - потеряется дробная часть
 //    return (int) Math.signum(price - o.price); // знак числа (отриц. в -1.0, полож. +1.0)
     return Double.compare(price, o.price);
   }
 
-  // определение метода compareTo() должно приводить к переопределению метода equals()
-  // переопределение метода equals() должно приводить к переопределению метода hashCode()
-  // эти три метода должны быть консистентными - вести себя одинаково
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Pizza other)) {
+      return false;
+    }
+    return price == other.price &&
+        name.equals(other.name) &&
+        size.equals(other.size);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, size, price);
+  }
 }
