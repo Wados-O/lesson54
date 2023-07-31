54fd10f204a040a33bf60b806b89a15e06bdfe4b
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-07-31 20:45:47 +0200

    feat : add StudentsDateBase.printMenu()

--- src/Students/StudentsDateBase.java
+++ src/Students/StudentsDateBase.java
@@ -8,26 +8,35 @@ public class StudentsDateBase {
 
   public static void main(String[] args) throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("Список команд");
+    printMenu();
     String command = reader.readLine();
-    while (!command.equalsIgnoreCase("выход")){
-      switch (command.toLowerCase()){
+    while (!command.equalsIgnoreCase("выход")) {
+      switch (command.toLowerCase()) {
         case "добавить":
           System.out.println("Добавляем студента");
           break;
-          case "сохранить":
-            System.out.println("Сохраняем список");
+        case "сохранить":
+          System.out.println("Сохраняем список");
           break;
-          case "вывод":
-            System.out.println("Выводим список на экран");
+        case "вывод":
+          System.out.println("Выводим список на экран");
           break;
         default:
           System.out.println(" Неизвестная команда " + command);
           break;
       }
-      System.out.println("Список команд");
+
+      printMenu();
       command = reader.readLine();
     }
+
+  }
+
+  private static void printMenu() {
+    System.out.println("- 'Добавить' студента");
+    System.out.println("- 'Сохранить' список студентов в файл");
+    System.out.println("- 'Вывод' списка студентов на экран");
+    System.out.println("- 'Выход' из программы");
 
   }
 }
