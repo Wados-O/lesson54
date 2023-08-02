69fee9912c0ab52897a914039f286ee5b8ff6e7e
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-02 14:29:30 +0200

    feat: homework add Books to main

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -1,11 +1,24 @@
 package homework;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) {
     List<Book> books = new ArrayList<>();
+    books.add(new Book("Book1","author1",132));
+    books.add(new Book("Book2","author1",96));
+    books.add(new Book("Book3","author1",12));
+    books.add(new Book("Book4","author1",152));
+    books.add(new Book("Book5","author1",142));
+    books.add(new Book("Book6","author1",123));
+    System.out.println("До сортировки:  ");
+    for (Book b : books){
+      System.out.println(b);
+    }
+
+
   }
 }
